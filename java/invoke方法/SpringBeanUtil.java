51d7f60783cbce11b60475cde20d459989de3258
cugb-zwx <956040604@qq.com>
2020-01-09 09:32:00 +0800

    invoke方法

---
+++ java/invoke方法/SpringBeanUtil.java
@@ -0,0 +1,132 @@
+package com.sec.eac.base.utils;
+
+import com.sec.eac.base.synchronous.result.SyncResult;
+import com.sec.eac.base.synchronous.utils.ResultUtil;
+import com.sec.eac.component.SpringContext;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Title:
+ * Project: DTS
+ * Description:
+ * Date: 2020-1-2
+ * Copyright: Copyright (c) 2020
+ * Company: 北京中科院软件中心有限公司 (SEC)
+ *
+ * @author zwx
+ * @version 1.0
+ */
+
+public class SpringBeanUtil {
+
+    public static <T> T get(String beanName) {
+        return (T) SpringContext.getObject(beanName);
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBeanUtil.class);
+    private static ConcurrentMap<String, Method> methodCache = new ConcurrentHashMap<>();
+
+    /**
+     * 通过反射执行指定方法
+     *
+     * @param beanName   指定 spring bean
+     * @param methodName 指定方法名
+     * @param params     指定参数
+     * @return ret            执行结果
+     */
+    public static SyncResult invokeMethod(String beanName, String methodName, Object... params) {
+        if (StringUtils.isBlank(beanName)) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 未指定");
+            return ResultUtil.returnFail("spring bean 未指定");
+        }
+        Object object;
+        try {
+            object = SpringContext.getObject(beanName);
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 获取失败", e);
+            return ResultUtil.returnFail("spring bean 获取失败");
+        }
+        if (object == null) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 未获取");
+            return ResultUtil.returnFail("spring bean 未获取");
+        }
+        Class clazz = object.getClass();
+
+        if (StringUtils.isBlank(methodName)) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 未指定");
+            return ResultUtil.returnFail("method 未指定");
+        }
+        Method method;
+        try {
+            method = getInvokeMethod(beanName, methodName, params, clazz.getDeclaredMethods());
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 获取失败", e);
+            return ResultUtil.returnFail("method 获取失败");
+        }
+        if (method == null) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 未获取");
+            return ResultUtil.returnFail("method 未获取");
+        }
+        try {
+            return ResultUtil.returnSuccess(method.invoke(object, params));
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----执行失败", e);
+            return ResultUtil.returnFail("执行失败");
+        }
+    }
+
+    private static Method getInvokeMethod(String beanName, String methodName, Object[] params, Method[] methods) {
+        String cacheKey = getCacheKey(beanName, methodName, params);
+        Method method = methodCache.get(cacheKey);
+        if (method == null) {
+            for (Method m : methods) {
+                if (m.getName().equals(methodName) && isParentClass(SyncResult.class, m.getReturnType())) {
+                    Class<?>[] pClassArray = m.getParameterTypes();
+                    if (pClassArray.length == params.length) {
+                        boolean mark = true;
+                        for (int i = 0; i < pClassArray.length; i++) {
+                            if (!isParentClass(pClassArray[0], params[i].getClass())) {
+                                mark = false;
+                                break;
+                            }
+                        }
+                        if (mark) {
+                            method = m;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (method != null) {
+                methodCache.put(cacheKey, method);
+            }
+        }
+        return method;
+    }
+
+    private static String getCacheKey(String beanName, String methodName, Object[] params) {
+        if (StringUtils.isBlank(beanName) || StringUtils.isBlank(methodName)) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder().append(beanName).append("-").append(methodName);
+        if (params != null) {
+            for (int i = 0; i < params.length; i++) {
+                sb.append("-").append(params[i].getClass().getName());
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean isParentClass(Class<?> pClass, Class<?> cClass) {
+        if (pClass != null && cClass != null && pClass.isAssignableFrom(cClass)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ java/invoke方法/SyncSpringUtil.java
@@ -0,0 +1,116 @@
+package com.sec.eac.base.synchronous.utils;
+
+import com.sec.eac.base.synchronous.result.SyncResult;
+import com.sec.eac.component.SpringContext;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Title:
+ * Project: DTS
+ * Description: 调用Spring IOC中的方法
+ * Date: 2019-11-11
+ * Copyright: Copyright (c) 2020
+ * Company: 北京中科院软件中心有限公司 (SEC)
+ *
+ * @author zwx
+ * @version 1.0
+ */
+@Component
+//@Transactional
+public class SyncSpringUtil {
+    private static final Logger log = LoggerFactory.getLogger(SyncSpringUtil.class);
+    private static ConcurrentMap<String, Method> methodCache = new ConcurrentHashMap<>();
+
+    /**
+     * 通过反射执行指定方法
+     *
+     * @param beanName   指定 spring bean
+     * @param methodName 指定方法名
+     * @param param      指定参数
+     * @return ret            执行结果
+     */
+    public static SyncResult invokeMethod(String beanName, String methodName, Object param) {
+        if (StringUtils.isBlank(beanName)) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 未指定");
+            return ResultUtil.returnFail("spring bean 未指定");
+        }
+        Object object;
+        try {
+            object = SpringContext.getObject(beanName);
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 获取失败", e);
+            return ResultUtil.returnFail("spring bean 获取失败");
+        }
+        if (object == null) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，spring bean 未获取");
+            return ResultUtil.returnFail("spring bean 未获取");
+        }
+        Class clazz = object.getClass();
+
+        if (StringUtils.isBlank(methodName)) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 未指定");
+            return ResultUtil.returnFail("method 未指定");
+        }
+        Method method;
+        try {
+            method = getInvokeMethod(beanName, methodName, param, clazz.getDeclaredMethods());
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 获取失败", e);
+            return ResultUtil.returnFail("method 获取失败");
+        }
+        if (method == null) {
+            log.error("任务[" + beanName + ":" + methodName + "]----未执行，method 未获取");
+            return ResultUtil.returnFail("method 未获取");
+        }
+        try {
+            return (SyncResult) method.invoke(object, param);
+        } catch (Exception e) {
+            log.error("任务[" + beanName + ":" + methodName + "]----执行失败", e);
+            return ResultUtil.returnFail("执行失败");
+        }
+    }
+
+    private static Method getInvokeMethod(String beanName, String methodName, Object param, Method[] methods) {
+        String cacheKey = getCacheKey(beanName, methodName, param.getClass().getName());
+        Method method = methodCache.get(cacheKey);
+        if (method == null) {
+            for (Method m : methods) {
+                if (m.getName().equals(methodName) && isParentClass(SyncResult.class, m.getReturnType())) {
+                    Class<?>[] pClassArray = m.getParameterTypes();
+                    if (pClassArray.length == 1) {
+                        if (isParentClass(pClassArray[0], param.getClass())) {
+                            method = m;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (method != null) {
+                methodCache.put(cacheKey, method);
+            }
+        }
+        return method;
+    }
+
+    private static String getCacheKey(String beanName, String methodName, String paramType) {
+        if (StringUtils.isBlank(beanName) || StringUtils.isBlank(methodName) || StringUtils.isBlank(paramType)) {
+            return null;
+        }
+        return new StringBuilder().append(beanName).append("-").append(methodName).append("-").append(paramType).toString();
+    }
+
+    private static boolean isParentClass(Class<?> pClass, Class<?> cClass) {
+        if (pClass != null && cClass != null && pClass.isAssignableFrom(cClass)) {
+            return true;
+        }
+        return false;
+    }
+}
